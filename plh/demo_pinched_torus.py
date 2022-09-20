0212262a4db84bd0d77d4e395860e80758c43b9b
Bastian Rieck <bastian@rieck.me>
2022-09-20 15:40:57 +0200

    Preparing demo for pinched torus calculation

--- plh/demo_pinched_torus.py
+++ plh/demo_pinched_torus.py
@@ -1,10 +1,13 @@
 import numpy as np
+import sys
 
 from math import pi
 from math import cos
 from math import sin
 
 from plh.euclidicity import Euclidicity
+
+from tqdm import tqdm
 
 n = 4096
 m = 512
@@ -39,13 +42,20 @@ for i in range(m):
 X = np.vstack((X, Y, Z)).T
 
 rng = np.random.default_rng(42)
-indices = rng.choice(X.shape[0], 1000, replace=False)
-X = X[indices]
 
-euclidicity = Euclidicity(0.05, 0.45, 0.2, 0.6, 2, n_steps=10, method="ripser")
+X = X[rng.choice(X.shape[0], 10000, replace=False)]
+query_points = X[rng.choice(X.shape[0], 1000, replace=False)]
 
-for x in X:
+euclidicity = Euclidicity(
+    0.05, 0.45, 0.2, 0.6, 2, n_steps=10, method="ripser", X=X
+)
+
+for x in tqdm(query_points, desc="Point"):
     values = euclidicity(X, x)
     score = np.mean(np.nan_to_num(values))
 
-    print(*x, score)
+    s = " ".join(str(a) for a in x)
+    s += f" {score}"
+    tqdm.write(s)
+
+    sys.stdout.flush()
