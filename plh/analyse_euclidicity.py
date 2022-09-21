690581e5caf72038952f79e6a5083f71717c290f
Bastian Rieck <bastian@rieck.me>
2022-09-21 09:50:15 +0200

    Added embedding

--- plh/analyse_euclidicity.py
+++ plh/analyse_euclidicity.py
@@ -4,6 +4,8 @@ import argparse
 
 import numpy as np
 
+from sklearn.manifold import MDS
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -17,10 +19,16 @@ if __name__ == "__main__":
     n_files = len(args.FILE)
     fig, axes = plt.subplots(ncols=n_files)
 
+    if n_files == 1:
+        axes = [axes]
+
+    emb = MDS()
+
     for filename, ax in zip(args.FILE, axes):
         X = np.loadtxt(filename)
         euclidicity = X[:, -1].flatten()
 
-        sns.boxplot(euclidicity, ax=ax)
+        X_emb = emb.fit_transform(X)
+        sns.scatterplot(x=X_emb[:, 0],  y=X_emb[:, 1], hue=euclidicity)
 
     plt.show()
