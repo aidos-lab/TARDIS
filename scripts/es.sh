187f64b8c2a9564ca2457920b3798da9dbe73489
Bastian Rieck <bastian.rieck@helmholtz-muenchen.de>
2022-09-21 11:10:54 +0200

    Updated easy submission script

---
+++ scripts/es.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# es.sh: easy submit --- submit a job quickly to the cluster
+#
+# This script tries to provide a 'fire and forget' solution for
+# submitting CPU jobs to the cluster. The parameters may not be
+# perfect, but it's probably sufficient for most cases.
+
+NAME="$1"
+CMD="$2"
+
+if [ -z "$2" ]; then
+  NAME="easy-submit"
+  CMD=$1
+fi
+
+if [ -z "$CMD" ]; then
+  echo "Usage: $0 [NAME] COMMAND"
+  echo "  Specify at least the command to run."
+  exit -1
+fi
+
+MAIL_USER=${USER}@helmholtz-muenchen.de
+
+sbatch -p cpu_p                 \
+       -J ${NAME}               \
+       -o "${NAME}_%j.out"      \
+       --cpus-per-task=32       \
+       --mem=8G                 \
+       --mail-type=END,FAIL     \
+       --mail-user=${MAIL_USER} \
+       --nice=10000             \
+       --wrap "${CMD}"
