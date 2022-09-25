24108e9a6b13ce1be79515eeda37bffed270928b
Bastian Rieck <bastian@rieck.me>
2022-09-25 14:46:18 +0200

    Added outlier criterion

--- plh/analyse_euclidicity.py
+++ plh/analyse_euclidicity.py
@@ -8,6 +8,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
+def detect_outliers(data):
+    """Detect outliers based on IQR criterion."""
+    # Simple outlier detection: clip everything that is larger than
+    # q3 + 1.5 * IQR.
+    iqr = np.subtract(*np.percentile(data, [75, 25]))
+    q1 = np.percentile(data, 25)
+    q3 = np.percentile(data, 75)
+
+    upper = (data > q3 + 1.5 * iqr)
+    lower = (data < q1 - 1.5 * iqr)
+
+    print("- Found", upper.sum(), "upper outliers")
+    print("- Found", lower.sum(), "lower outliers")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("FILE", nargs="+", help="Input filename(s)")
@@ -23,6 +38,8 @@ if __name__ == "__main__":
     ) in enumerate(args.FILE):
         X = np.loadtxt(filename)
         euclidicity = X[:, -1].flatten()
+
+        detect_outliers(euclidicity)
 
         sns.histplot(data=euclidicity, kde=True, ax=axes[0, col])
         sns.violinplot(data=euclidicity, ax=axes[1, col])
