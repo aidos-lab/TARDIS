5701766ca0a893b863ba77f790524635b253e383
Bastian Rieck <bastian@rieck.me>
2022-11-30 10:53:22 +0100

    Added (hyper)sphere sampling function

--- toast/shapes.py
+++ toast/shapes.py
@@ -46,6 +46,56 @@ def sample_from_annulus(n, r, R, seed=None):
     return X
 
 
+def sample_from_sphere(n=100, d=2, r=1, noise=None, seed=None):
+    """Sample `n` data points from a `d`-sphere in `d + 1` dimensions.
+
+    Parameters
+    -----------
+    n : int
+        Number of data points in shape.
+
+    d : int
+        Dimension of the sphere.
+
+    r : float
+        Radius of sphere.
+
+    noise : float or None
+        Optional noise factor. If set, data coordinates will be
+        perturbed by a standard normal distribution, scaled by
+        `noise`.
+
+    seed : int, instance of `np.random.Generator`, or `None`
+        Seed for the random number generator, or an instance of such
+        a generator. If set to `None`, the default random number
+        generator will be used.
+
+    Returns
+    -------
+    np.array of shape `(n, d + 1)`
+        Array of sampled coordinates.
+
+    Notes
+    -----
+    This function was originally authored by Nathaniel Saul as part of
+    the `tadasets` package. [tadasets]_
+
+    References
+    ----------
+    .. [tadasets] https://github.com/scikit-tda/tadasets
+    """
+    rng = np.random.default_rng(seed)
+    data = rng.standard_normal((n, d+1))
+
+    # Normalize points to the sphere
+    data = r * data / np.sqrt(np.sum(data**2, 1)[:, None])
+
+    if noise:
+        data += noise * rng.standard_normal(data.shape)
+
+    return np.asarray(data)
+
+
 def sample_from_wedged_spheres(n=100, d=2, r=1, noise=None, seed=None):
     """Sample points from two wedged spheres.
 
