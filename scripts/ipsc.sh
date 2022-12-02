e84b6a929346cf995daca23c77b712c7950d9a42
Bastian Rieck <bastian.rieck@helmholtz-muenchen.de>
2022-12-02 08:04:50 +0100

    Updated submission script for `ipsc` data

--- scripts/ipsc.sh
+++ scripts/ipsc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-for POINTS in 2500 5000 10000; do
-  ./es.sh IPSC-${POINTS} "poetry run python ../toast/cli.py ../data/ipsc.npz --seed 42 -q ${POINTS} -d 16 > ../output/ipsc_d16_q${POINTS}_seed42.txt"
+for POINTS in 1000 2500 5000 10000; do
+  ./es.sh IPSC-${POINTS} "poetry run python ../toast/cli.py ../data/ipsc.npz --seed 42 -q ${POINTS} -d 16 -o ../output/ipsc_d16_q${POINTS}_seed42.csv"
 done
