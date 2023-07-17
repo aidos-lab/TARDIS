9dc1a29a82acd885758c6057e3b64db1b66d5055
Bastian Rieck <bastian@rieck.me>
2023-07-17 17:53:10 -0400

    Configured documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,27 +1,33 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+project = "TARDIS"
+copyright = "2023, Julius von Rohrscheidt and Bastian Rieck"
+author = "Julius von Rohrscheidt and Bastian Rieck"
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.linkcode",
+]
 
-project = 'TARDIS'
-copyright = '2023, Julius von Rohrscheidt and Bastian Rieck'
-author = 'Julius von Rohrscheidt and Bastian Rieck'
+# Ensure that member functions are documented. These are sane defaults.
+autodoc_default_options = {
+    "members": True,
+    "member-order": "bysource",
+    "special-members": "__init__",
+    "undoc-members": True,
+    "exclude-members": "__weakref__",
+}
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
-
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
+# Tries to assign some semantic meaning to arguments provided with
+# single backtics, such as `x`. This way, we can ignore `func` and
+# `class` targets etc. (They still work, though!)
+default_role = "obj"
 
+html_theme = "alabaster"
+html_static_path = ["_static"]
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
+# Ensures that modules are sorted correctly. Since they all pertain to
+# the same package, the prefix itself can be ignored.
+modindex_common_prefix = ["tardis."]
